5d4c4da94acb98acbc30fda1568a8c6ec48ca0ad
MmantisS <mantis2000@mail.ru>
2023-06-16 19:33:58 +0500

    create consts, data classification creation

---
+++ consts.py
@@ -0,0 +1,18 @@
+RANDOM_STATE = 1337
+
+DATA_TEST_SIZE = .3
+DATA_N = 10_000
+
+Y_TRAIN = './train/'
+Y_TRAIN_FULL = Y_TRAIN + 'y.pckl'
+X_TRAIN = './train/'
+X_TRAIN_FULL = X_TRAIN + 'x.pckl'
+Y_TEST = './test/'
+Y_TEST_FULL = Y_TEST + 'y.pckl'
+X_TEST = './test/'
+X_TEST_FULL = X_TEST + 'x.pckl'
+
+DATA_PATH = './data_default/' + 'data.csv'
+
+MODEL = './model/'
+MODEL_FULL = MODEL + 'model.pckl'
