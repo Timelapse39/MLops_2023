87fe116d7a65e108d9b4f676c1be96ce20992c65
MmantisS <mantis2000@mail.ru>
2023-06-16 20:09:37 +0500

    Create model_testing

---
+++ model_testing.py
@@ -0,0 +1,13 @@
+from sklearn.metrics import r2_score
+import pickle
+import consts
+def load_data(full_path: str) -> Any:
+   return pickle.load(open(full_path, "rb"))
+
+model = load_data(consts.MODEL_FULL)
+x_test = load_data(consts.X_TEST_FULL)
+y_test = load_data(consts.Y_TEST_FULL)
+
+y_pred = model.predict(x_test)
+score = r2_score(y_test, y_pred)
+print('Coefficient of determination score: ', score)
